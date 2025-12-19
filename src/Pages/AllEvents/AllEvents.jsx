c58c9f8dbf6eee61a4c2b9889364314eba4a1b77
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2025-12-19 20:08:12 +0600

    event search functionality added

--- src/Pages/AllEvents/AllEvents.jsx
+++ src/Pages/AllEvents/AllEvents.jsx
@@ -7,42 +7,70 @@ import { MoonLoader } from "react-spinners";
 import EventCard from "../../Components/Shared/EventCard/EventCard";
 
 const AllEvents = () => {
-    const [sort,setSort]=useState('eventDate')
-    const [order,setOrder]=useState('desc')
+  const [sort, setSort] = useState("eventDate");
+  const [order, setOrder] = useState("");
+  const [filter, setFilter] = useState("");
+  const [search, setSearch] = useState("");
   const { data: allEvents, isLoading: eventsLoading } = useQuery({
-    queryKey: ["allEvents",sort,order],
+    queryKey: ["allEvents", sort, order, filter, search],
     queryFn: async () => {
-      const res = await axios.get(`http://localhost:4000/all-events?sort=${sort}&order=${order}`);
+      const res = await axios.get(
+        `http://localhost:4000/all-events?sort=${sort}&order=${order}&filter=${filter}&search=${search}`
+      );
       return res.data;
     },
   });
 
-  const handleSort=(e)=>{
-const sortText=e.target.value.split('-')
-setSort(sortText[0])
-setOrder(sortText[1])
+  //   all events category
+  const { data: allEventCategories = [], isLoading: categoryLoading } =
+    useQuery({
+      queryKey: ["eventCategories"],
+      queryFn: async () => {
+        const res = await axios.get(`http://localhost:4000/event-categories`);
+        return res.data;
+      },
+    });
 
-  }
+  const eventCategories = allEventCategories.map((cat) => cat.category);
+  const categories = [...new Set(eventCategories)];
 
-  console.log(allEvents);
+  const handleSort = (e) => {
+    const sortText = e.target.value.split("-");
+    setSort(sortText[0]);
+    setOrder(sortText[1]);
+  };
+
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearch(e.target.search.value.trim());
+  };
 
   return (
     <Container>
       <div className="flex flex-col-reverse sm:flex-row sm:justify-between justify-center items-center my-10 gap-y-5 ">
         <div className="flex items-center+ gap-5">
-            <h3>All Clubs ({eventsLoading ? <span>...</span> : allEvents.length})</h3>
+          <h3>
+            All Clubs ({eventsLoading ? <span>...</span> : allEvents.length})
+          </h3>
           <button className="cursor-pointer text-secondary hover:text-primary text-xl">
             <LuRefreshCw></LuRefreshCw>{" "}
           </button>
         </div>
         {/* search */}
 
-        <form className="flex justify-center items-center">
+        <form
+          onSubmit={handleSearch}
+          className="flex justify-center items-center"
+        >
           <div className="relative h-[45px]">
             <input
               type="text"
               name="search"
-              placeholder="Search By Name..."
+              placeholder="Search By Events Name..."
               spellCheck={false}
               className="bg-gray-200 w-full sm:w-[400px] px-5 pr-24 py-2 rounded-4xl h-full  focus:outline-2 focus:outline-secondary"
               required
@@ -64,7 +92,6 @@ setOrder(sortText[1])
             onChange={(value) => handleSort(value)}
             className="select w-[180px] "
           >
-          
             <option value={"eventDate-desc"}>Newest First</option>
             <option value={"eventDate-asc"}>Lowest First</option>
             <option value={"eventFee-desc"}>Highest Fee</option>
@@ -75,13 +102,13 @@ setOrder(sortText[1])
         <div className="flex">
           <label className="label mr-3">Filter By</label>
           <select
-            // onChange={(value) => handleFilter(value)}
+            onChange={(value) => handleFilter(value)}
             className="select w-[180px]"
           >
             <option disabled={true}>Filter by Category</option>
             <option value="">All Categories</option>
 
-            {/* {!allCategoriesLoading &&
+            {!categoryLoading &&
               categories.map((category, index) => (
                 <option
                   className="capitalize"
@@ -90,28 +117,27 @@ setOrder(sortText[1])
                 >
                   {category}
                 </option>
-              ))} */}
+              ))}
           </select>
         </div>
       </div>
-<div className="mb-20">
-    
-            {eventsLoading ? (
-        <div className="grid justify-center h-[350px] mt-10">
-          <MoonLoader size={30} speedMultiplier={0.75} color="#22C55E" />
-        </div>
-      ) : allEvents.length === 0 ? (
-        <h2 className="font-semibold text-center text-xl text-error my-20">
-          No event available now.
-        </h2>
-      ) : (
-        <div className="grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {allEvents.map((event) => (
-            <EventCard key={event?._id} event={event}></EventCard>
-          ))}
-        </div>
-      )}
-</div>
+      <div className="mb-20">
+        {eventsLoading ? (
+          <div className="grid justify-center h-[350px] mt-10">
+            <MoonLoader size={30} speedMultiplier={0.75} color="#22C55E" />
+          </div>
+        ) : allEvents.length === 0 ? (
+          <h2 className="font-semibold text-center text-xl text-error my-20">
+            No event available now.
+          </h2>
+        ) : (
+          <div className="grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {allEvents.map((event) => (
+              <EventCard key={event?._id} event={event}></EventCard>
+            ))}
+          </div>
+        )}
+      </div>
     </Container>
   );
 };
